a61999f9c2f118e9f6559ace88a50c134882a2b7
Oraclesid1411 <giloracle76@gmail.com>
2025-06-21 08:39:56 +0000

    Initial push: frontend Next.js

--- config/staticip.js
+++ config/staticip.js
@@ -1,2 +1,8 @@
-export const StaticIP =  "http://localhost:4001/"
+// export const StaticIP =  "http://localhost:4001/"
+// export const StaticIP = process.env.NEXT_PUBLIC_API_URL;
 //export const StaticIP =  "http://77.37.125.3:4001/"
+
+export const StaticIP =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4001/' // local
+    : process.env.NEXT_PUBLIC_API_URL; // prod (Railway)
